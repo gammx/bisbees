f592b3d4f45169fe25db78576bc6031f56697c3c
gammx <dakdaker55@gmail.com>
2022-11-22 11:38:53 -0700

    update: make marquee responsive

--- components/SponsorMarquee.tsx
+++ components/SponsorMarquee.tsx
@@ -1,11 +1,15 @@
 import Marquee from "react-fast-marquee";
 import sponsors from '../utils/sponsors';
+import { useMediaQuery } from "react-responsive";
 
 const SponsorMarquee = () => {
+	const isNotMobile = useMediaQuery({ minWidth: 640 });
+
 	return (
 		<div className="px-6 sm:px-14 md:px-24 xl:px-40 pt-10 pb-16">
 			<Marquee
 				pauseOnHover
+				gradientWidth={isNotMobile ? 200 : 50}
 			>
 				{sponsors.map((sponsor, index) => (
 					<a key={index} href={sponsor.href} target="_blank" className="mx-6">

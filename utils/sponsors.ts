59a28180e415bb1c6cd4a677666935b0e9d22e2c
gammx <dakdaker55@gmail.com>
2022-11-22 12:22:32 -0700

    add: complete sponsors list

--- utils/sponsors.ts
+++ utils/sponsors.ts
@@ -77,17 +77,82 @@ export default [
 	},
 	{
 		icon: '/sponsors/seakeeper.png',
-		name: 'SeaKeeper',
+		name: "SeaKeeper",
 		href: 'https://www.seakeeper.com/',
 	},
 	{
 		icon: '/sponsors/mauijim.png',
-		name: 'Maui Jim',
+		name: "Maui Jim",
 		href: 'https://www.mauijim.com/',
 	},
 	{
 		icon: '/sponsors/sevenstar.png',
 		name: "SevenStar Yacht Transport",
 		href: 'https://www.sevenstar-yacht-transport.com/',
-	}
+	},
+	{
+		icon: '/sponsors/arnian.png',
+		name: "Arnian Group",
+		href: 'http://www.arnian.com/en/',
+	},
+	{
+		icon: '/sponsors/bajavida.png',
+		name: "Baja Vida",
+		href: 'https://www.bajavida.com/',
+	},
+	{
+		icon: '/sponsors/bisbeesfund.png',
+		name: "Bisbee's Conservation Fund",
+		href: 'https://bisbeesconservationfund.org/',
+	},
+	{
+		icon: '/sponsors/boaterslist.png',
+		name: "Boaters List",
+		href: 'https://www.boaterslist.com/',
+	},
+	{
+		icon: '/sponsors/bomberosbcs.png',
+		name: "Bomberos Voluntarios de Cabo San Lucas",
+		href: 'https://www.bomberoscsl.com/',
+	},
+	{
+		icon: '/sponsors/childrensfoundation.png',
+		name: "Los Cabos Children's Foundation",
+		href: 'https://www.loscaboschildren.org/bisbees',
+	},
+	{
+		icon: '/sponsors/hiltons.png',
+		name: "Hilton's",
+		href: 'http://realtime-navigator.com/',
+	},
+	{
+		icon: '/sponsors/nationalrental.png',
+		name: 'National Car Rental',
+		href: 'https://nationalcar.com.mx/partners/bisbees',
+	},
+	{
+		icon: '/sponsors/nativespirit.png',
+		name: "Native Spirit Tequila",
+		href: 'https://nativespirittequila.com/',
+	},
+	{
+		icon: '/sponsors/pelagic.png',
+		name: "Pelagic Gear",
+		href: 'https://pelagicgear.com/',
+	},
+	{
+		icon: '/sponsors/peninsularcreations.png',
+		name: "Peninsular Creations",
+		href: 'https://peninsularcreations.com/',
+	},
+	{
+		icon: '/sponsors/scalemaster.png',
+		name: "Scale Master",
+		href: 'https://www.iws-scalemaster.com/',
+	},
+	{
+		icon: '/sponsors/turismobcs.png',
+		name: "Secretaría de Turismo y Economía de Baja California Sur",
+		href: 'https://setuesbcs.gob.mx/',
+	},
 ] as Sponsor[];
